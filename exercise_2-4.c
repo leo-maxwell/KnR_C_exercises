204b921315d56bb4930c4399fd4c598f10b5ed5a
Leo Maxwell <Leo_Maxwell@outlook.com>
2026-05-22 20:12:10 +0930

    1st v. of e2-4

---
+++ exercise_2-4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void squeeze(char s1[], char s2[]) {
+    int i = 0, j = 0, k = 0;
+    bool has;
+    while (s1[i] != '\0') {
+        k = 0;
+        has = false;
+        while (s2[k] != '\0') {
+            if (s1[i] == s2[k]) {
+                has = true;
+                break;
+            }
+        }
+        if (!has) {
+            s1[j++] = s1[i];
+        }
+        i++;
+    }
+}
+
+int main() {
+    char s1[] = "Hello World!";
+    char s2[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    squeeze(s1, s2);
+    printf("%s", s1);
+}
