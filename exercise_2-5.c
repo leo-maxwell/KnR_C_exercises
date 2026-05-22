9ce410ba79e57672c748d901b58ac365bf6f337e
Leo Maxwell <Leo_Maxwell@outlook.com>
2026-05-22 20:19:20 +0930

    1st v. of e2-5

---
+++ exercise_2-5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int any(char s1[], char s2[]) {
+    int i = 0, j = 0, k = 0;
+    while (s1[i] != '\0') {
+        k = 0;
+        while (s2[k++] != '\0') {
+            if (s1[i] == s2[k]) {
+                return i;
+            }
+        }
+        i++;
+    }
+    return -1;
+}
+
+int main() {
+    char s1[] = "Hello World!";
+    char s2[] = "/.,;'[]!";
+    printf("%d\n", any(s1, s2));
+}
