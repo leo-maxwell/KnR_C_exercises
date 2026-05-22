969165e0a048b08e4000793139dd64159b9c0814
Leo Maxwell <Leo_Maxwell@outlook.com>
2026-05-22 15:48:47 +0930

    improved clean script to avoid misdelete

--- clean.sh
+++ clean.sh
@@ -1,1 +1,11 @@
-find . -type f ! -name '*.c' ! -name '*.sh' -delete
+find . \
+  -type f \
+  ! -path '*/.git/*' \
+  ! -name '.gitignore' \
+  -exec sh -c '
+    for f do
+      if [ "$(file -b --mime-encoding -- "$f")" = "binary" ]; then
+        rm -- "$f"
+      fi
+    done
+  ' sh {} +
