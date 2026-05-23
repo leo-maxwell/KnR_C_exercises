908ea3b3157be11460d5aaf6ada239eb1ce6e3e1
Leo Maxwell <Leo_Maxwell@outlook.com>
2026-05-23 16:16:34 +0930

    v1 of e3-1

---
+++ exercise_3-1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int binsearch(int x, int v[], int n) {
+    int low, high, mid;
+    low = 0;
+    high = n-1;
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (x < v[mid]) {
+            high = mid - 1;
+        } else if (x > v[mid]) {
+            low = mid + 1;
+        } else
+            return mid;
+    }
+    return -1;
+}
+
+int binsearch2(int x, int v[], int n) {
+    int low, high, mid;
+    low = 0;
+    high = n-1;
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (x < v[mid]) {
+            high = mid - 1;
+        } else
+            low = mid + 1;
+    }
+    if (v[mid] == x) return mid;
+    return -1;
+}
+
+int main() {
+    int v[] = {1, 3, 4, 6, 7, 9, 10, 13, 17, 19, 23, 25};
+    printf("%d\n", binsearch(13, v, 12));
+    printf("%d\n", binsearch2(13, v, 12));
+}
