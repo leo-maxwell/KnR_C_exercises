4e6e094bed9954d44e8da3821e9699c62588719e
Leo Maxwell <Leo_Maxwell@outlook.com>
2026-05-24 18:29:37 +0930

    v2 of e4-2.c

--- exercise_4-2.c
+++ exercise_4-2.c
@@ -2,28 +2,28 @@
 #include <math.h>
 
 double atof(char s[]) {
-    double val, power = 1.0, exp_val = 0;
-    int i, sign, exp_sign;
+    double val, power = 1.0;
+    int i, sign, exp_sign, exp_val = 0;
     for (i = 0; isspace(s[i]); i++);
     sign = (s[i] == '-') ? -1 : 1;
     if (s[i] == '+' || s[i] == '-') i++;
     for (val = 0.0; isdigit(s[i]); i++)
         val = 10.0 * val + (s[i] - '0');
     if (s[i] == '.') {
-            i++;
-            for (power = 1.0; isdigit(s[i]); i++) {
-                val = 10.0 * val + (s[i] - '0');
-                power *= 10.0;
-            }
+        i++;
+        for (power = 1.0; isdigit(s[i]); i++) {
+            val = 10.0 * val + (s[i] - '0');
+            power *= 10.0;
         }
+    }
     if (s[i] == 'e' || s[i] == 'E') {
-            i++;
-            exp_sign = (s[i] == '-') ? -1 : 1;
-            if (s[i] == '+' || s[i] == '-') i++;
-            for (; isdigit(s[i]); i++) {
-                exp_val = 10.0 * exp_val + (s[i] - '0');
-            }
-            power = exp_sign > 0 ? power / pow(10, exp_val) : power * pow(10, exp_val);
+        i++;
+        exp_sign = (s[i] == '-') ? -1 : 1;
+        if (s[i] == '+' || s[i] == '-') i++;
+        for (; isdigit(s[i]); i++) {
+            exp_val = 10.0 * exp_val + (s[i] - '0');
         }
+        power = exp_sign > 0 ? power / pow(10, exp_val) : power * pow(10, exp_val);
+    }
     return sign * val / power;
 }
