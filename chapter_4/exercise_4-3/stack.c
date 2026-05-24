3508da71bd5182fac538c00ca50f1007bc504529
Leo Maxwell <Leo_Maxwell@outlook.com>
2026-05-25 07:51:20 +0930

    v2 of e4-3: fix variable name which is incompatible with intel asm symbs

--- chapter_4/exercise_4-3/stack.c
+++ chapter_4/exercise_4-3/stack.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include "calc.h"
 
-int sp = 0;
+int stack_p = 0;
 double stack[STACKSIZE];
 
 void push(double f) {
-    if (sp < STACKSIZE) {
-        stack[sp++] = f;
+    if (stack_p < STACKSIZE) {
+        stack[stack_p++] = f;
     } else {
         printf("push: stack full, can't push %g\n", f);
     }
 }
 
 double pop(void) {
-    if (sp > 0) {
-        return stack[--sp];
+    if (stack_p > 0) {
+        return stack[--stack_p];
     } else {
         printf("pop: stack empty");
         return 0.0;
